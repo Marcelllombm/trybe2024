7f8d722b5a64f21a45ea7e3d6f07c78ed0c0b534
Marcelo Borges <melombm@hotmail.com>
2024-04-17 13:54:56 -0300

    javascript exercise 3

--- 02_javascript_introduction/02-day-1-exercise.js
+++ 02_javascript_introduction/02-day-1-exercise.js
@@ -9,6 +9,7 @@ Divisão (num1 / num2)
 Módulo (num1 % num2)
 */
 
+console.log("////////// Exercício 1 ////////////////////");
 let numbOne = Math.floor(Math.random() * 100);
 let numbTwo = Math.floor(Math.random() * 100);
 
@@ -26,6 +27,25 @@ Utilize if/else para escrever um código que retorne o maior de dois números.
  Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
  */
 
+ console.log("////////// Exercício 2 ////////////////////");
  const numeroMaior = numbOne > numbTwo ? `o numero 1 e Maior ${numbOne}` : `o numero 2 e Maior ${numbTwo}`;
-
  console.log(numeroMaior);
+
+ /*
+ Exercício 3
+Utilize if/else para escrever um código que retorne o maior de três números.
+ Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+  */
+ 
+console.log("////////// Exercício 3 ////////////////////");
+const n1 = 9; const n2 = 3; const n3 = 7;
+let maiorDoTres = ""
+if(n1 > n2 && n1 > n3){
+    maiorDoTres = " o numero n1 e maior";
+} else if (n2 > n1 && n2 > n3){
+    maiorDoTres = "o numero n2  e maior";
+}else {
+    maiorDoTres = "o numero n3 e maior";
+}
+
+console.log(maiorDoTres);
