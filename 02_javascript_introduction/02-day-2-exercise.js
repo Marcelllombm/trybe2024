934598e284781b33a08b5d84adbd05047f5103a8
Marcelllombm <melombm@hotmail.com>
2024-04-23 20:23:36 -0300

    day 2 - javascript execise 4

--- 02_javascript_introduction/02-day-2-exercise.js
+++ 02_javascript_introduction/02-day-2-exercise.js
@@ -130,34 +130,3 @@ for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
 };
 
 
-console.log("////////// day - 2 #### Exercício 5 ////////////////////");
-/*
-Agora, inverta o lado do triângulo. Por exemplo:
-
-n = 5
-
-    *
-   **
-  ***
- ****
-*****
-
-*/
-
-let n = 5;
-let symbol = '*';
-let inputLine = '';
-let inputPosition = n - 1;
-
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
-  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
-    if (columnIndex < inputPosition) {
-      inputLine = inputLine + ' ';
-    } else {
-      inputLine = inputLine + symbol;
-    }
-  }
-  console.log(inputLine);
-  inputLine = '';
-  inputPosition -= 1;
-};
